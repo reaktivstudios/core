fe48052b8ac5320510c9d274dc78ee93fae7ac7a
Jay Hoffmann <jhoffm34@gmail.com>
2026-09-24 15:39:16 -0400

    CTA update

--- deprecated/inc/classes/Post_Type/CTAs.php
+++ deprecated/inc/classes/Post_Type/CTAs.php
@@ -7,32 +7,9 @@
 
 namespace RKV\Utilities\Post_Type;
 
+use RKV\Core\Post_Type\CTAs as CoreCTABase;
+
 /**
  * Define the CTA class and associated methods.
  */
-class CTAs extends Base {
-
-	/**
-	 * Post type name.
-	 *
-	 * @var string
-	 */
-	protected $post_type_name = 'rkv-cta';
-
-
-	/**
-	 * Initialize the post type.
-	 */
-	protected function initialize_post_type() {
-		$this->post_type_args = [
-			'label'     => __( 'CTAs', 'rkv-utilities' ),
-			'labels'    => [
-				'name'          => _x( 'CTAs', 'Post Type General Name', 'rkv-utilities' ),
-				'singular_name' => _x( 'CTA', 'Post Type Singular Name', 'rkv-utilities' ),
-			],
-			'supports'  => [ 'title', 'editor', 'revisions' ],
-			'public'    => false,
-			'menu_icon' => 'dashicons-megaphone',
-		];
-	}
-}
+class CTAs extends CoreCTABase {}
